9d5b98e3193032deb50049db5eb0e0acad27023f
ThanhNhanNguyen <thanhnhan.b12002@gmail.com>
2023-04-18 11:26:03 +1000

    connect to FireStore to read and write succefully

---
+++ app/src/main/java/com/example/shinigami/FbHelper.java
@@ -0,0 +1,72 @@
+package com.example.shinigami;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class FbHelper {
+    private FirebaseFirestore db;
+    private String TAG = "FbHelper";
+
+    public FbHelper(FirebaseFirestore db) {
+        this.db = db;
+    }
+
+    public void addUser(User u) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("id", u.getUserId());
+        user.put("firstName ", u.getFirstName());
+        user.put("lastName", u.getLastName());
+        user.put("dob", u.getDob());
+
+        // Add a new document with a generated ID
+        db.collection("Users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    public void getUsers () {
+//        DocumentReference document = db.collection("Users").document("AOgFm3Si1WXALt1DCj7o");
+//        document.get().addOnSuccessListener()
+        db.collection("Users")
+                .get ( )
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                } ) ;
+    }
+
+}
--- app/src/main/java/com/example/shinigami/MainActivity.java
+++ app/src/main/java/com/example/shinigami/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.shinigami;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import android.os.Bundle;
 import java.util.HashMap;
@@ -11,47 +12,34 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference userRef = db.collection("Users");
 
-    FirebaseFirestore db;
     private String TAG = "FireStore";
+    private FbHelper fbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example from Firebase
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
+        //make a FbHelper object
+        fbHelper = new FbHelper(db);
+        fbHelper.addUser(new User( 1,"Thanh Nhan", "Nguyen", "11-02-2002"));
+        fbHelper.addUser(new User( 2,"Hamas", "Massood", "11-02-2002"));
+        fbHelper.addUser(new User( 3,"Page", "Perret", "11-02-2002"));
 
-        // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
-
+        fbHelper.getUsers();
     }
-
-
 }
